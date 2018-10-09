66cd0137e19af9373ca98e42642a7630e6215622
Filippo Bonfante <bonfantefilippo@gmail.com>
2018-10-09 18:23:50 +0200

    gestione mqtt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { MqttModule, IMqttServiceOptions } from 'ngx-mqtt';
 
 export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  connectOnCreate: false
 };
 
 
--- src/app/home-content/home-content.component.ts
+++ src/app/home-content/home-content.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { MqttService, IMqttServiceOptions } from 'ngx-mqtt';
+import { MqttService, IMqttServiceOptions, IMqttMessage, IOnErrorEvent } from 'ngx-mqtt';
 
 @Component({
   selector: 'app-home-content',
@@ -9,7 +9,13 @@ import { MqttService, IMqttServiceOptions } from 'ngx-mqtt';
 })
 export class HomeContentComponent implements OnInit {
 
+
   private connOption: IMqttServiceOptions;
+  private measurement = 0;
+
+  private topic = "SYMulation/DataLogger/sensori";
+  private topicTest = "mytest/digit";
+  private formTopic;
 
   constructor(private _mqttService: MqttService) { }
 
@@ -19,20 +25,47 @@ export class HomeContentComponent implements OnInit {
   saveConnectionOptions(connectionForm: NgForm) {
     if (connectionForm.valid) {
       const formValue = connectionForm.value;
-      console.log(formValue.clientId);
+      this.formTopic = formValue.topic;
+      console.log(this.formTopic);
       this.connOption = {
           hostname: formValue.hostname,
-          port: formValue.port,
+          port: 3001,
           path: "",
           username: formValue.username, //admin
           password: formValue.password, //secret
           clientId: formValue.clientId,
           protocol: "wss"
       };
-      //console.log(this.connOption);
 
-    this._mqttService.connect(this.connOption);
-
+      this._mqttService.connect(this.connOption);
+      this.manageConnection();
     }
   }
+
+  manageConnection() {
+    this._mqttService.onConnect.subscribe(() => {
+      console.warn("Connesso al broker");
+
+      this._mqttService.observe(this.formTopic).subscribe((message: IMqttMessage) => {
+        console.log(`Sottoscritto topic: ${message.topic}`);
+        this.measurement = JSON.parse(message.payload.toString());
+         console.dir(this.measurement);
+      });
+
+    });
+
+    this._mqttService.onMessage.subscribe(() => {
+      console.warn("Ricevuto messaggio");
+    });
+
+    this._mqttService.onError.subscribe((error) => {
+      console.log("Errore di connesione.");
+    });
+
+    this._mqttService.onError.subscribe((error) => {
+      console.log("Errore di connesione.");
+    });
+  }
+
+
 }
